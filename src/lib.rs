ebd6e6c513c01d78499f7af919ea763bcf9d6291
Ilya Bogdanov <fumlead@gmail.com>
2025-02-08 16:02:47 +0400

    Expected parser output

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,145 @@
-pub struct Parser {}
+use std::ops::RangeInclusive;
 
-impl Parser {
-    pub fn new(_: &str) -> Self {
-        Self {}
+pub struct Parser<'a> {
+    input: &'a str,
+}
+
+impl<'a> Parser<'a> {
+    pub fn new(input: &'a str) -> Self {
+        Self { input }
     }
 
-    pub fn parse(&self) -> Result<(), ()> {
-        Ok(())
+    pub fn parse(&self) -> Result<Report<'a>, ()> {
+        let report = Report {
+            origin: self.input,
+            kind: ReportKind::Metar,
+            station: Station {
+                icao_code: "EHLE",
+                name: "Lelystad Airport".to_owned(),
+                country: "Germany".to_owned(),
+            },
+            time: Time {
+                day: 28,
+                hour: 9,
+                minute: 25,
+            },
+            is_correction: false,
+            is_auto: true,
+            wind: Wind {
+                direction: 210,
+                speed: Knots(9),
+                gust: Some(Knots(19)),
+                variable: Some(60..=130),
+            },
+            visibility: 5000,
+            percipitation: Some(Percipitation::Rain {
+                intensity: Some(Intensity::Light),
+            }),
+            clouds: vec![
+                CloudLayer {
+                    ceiling: 700,
+                    cover: Cover::Few,
+                    ..Default::default()
+                },
+                CloudLayer {
+                    ceiling: 1400,
+                    cover: Cover::Broken,
+                    significant: Some(CloudSignificant::Cumulonimbus),
+                },
+                CloudLayer {
+                    ceiling: 1700,
+                    cover: Cover::Broken,
+                    ..Default::default()
+                },
+            ],
+            temperature: 2,
+            dew_point: -1,
+            pressure: Hectopascal(1001),
+        };
+        Ok(report)
     }
 }
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Report<'a> {
+    pub origin: &'a str,
+    pub kind: ReportKind,
+    pub station: Station<'a>,
+    pub time: Time,
+    pub is_correction: bool,
+    pub is_auto: bool,
+    pub wind: Wind,
+    pub visibility: u32,
+    pub percipitation: Option<Percipitation>,
+    pub clouds: Vec<CloudLayer>,
+    pub temperature: i32,
+    pub dew_point: i32,
+    pub pressure: Hectopascal,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Knots(pub u32);
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ReportKind {
+    Metar,
+    Speci,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Station<'a> {
+    pub icao_code: &'a str,
+    pub name: String,
+    pub country: String,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Time {
+    pub day: u8,
+    pub hour: u8,
+    pub minute: u8,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Wind {
+    pub direction: u32,
+    pub speed: Knots,
+    pub gust: Option<Knots>,
+    pub variable: Option<RangeInclusive<u32>>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Percipitation {
+    Rain { intensity: Option<Intensity> },
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Intensity {
+    Light,
+    Heavy,
+}
+
+#[derive(Default, Debug, PartialEq, Eq)]
+pub struct CloudLayer {
+    pub ceiling: u32,
+    pub cover: Cover,
+    pub significant: Option<CloudSignificant>,
+}
+
+#[derive(Default, Debug, PartialEq, Eq)]
+pub enum Cover {
+    #[default]
+    Clear,
+    Few,
+    Scattered,
+    Broken,
+    Overcast,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum CloudSignificant {
+    Cumulonimbus,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Hectopascal(pub u32);
--- tests/tests.rs
+++ tests/tests.rs
@@ -1,8 +1,60 @@
 use parser::Parser;
+use parser::{
+    CloudLayer, CloudSignificant, Cover, Hectopascal, Intensity, Knots, Percipitation, Report,
+    ReportKind, Station, Time, Wind,
+};
 
 #[test]
 fn primary() {
-    let input = "METAR EHLE 280925Z AUTO 21009G19KT 060V130 5000 -RA FEW007 BKN014CB BKN017 02/M01 Q1001 BECMG 6000";
+    let input =
+        "METAR EHLE 280925Z AUTO 21009G19KT 060V130 5000 -RA FEW007 BKN014CB BKN017 02/M01 Q1001";
     let parser = Parser::new(input);
     assert!(parser.parse().is_ok());
+    let expected = Report {
+        origin: input,
+        kind: ReportKind::Metar,
+        station: Station {
+            icao_code: "EHLE",
+            name: "Lelystad Airport".to_owned(),
+            country: "Germany".to_owned(),
+        },
+        time: Time {
+            day: 28,
+            hour: 9,
+            minute: 25,
+        },
+        is_correction: false,
+        is_auto: true,
+        wind: Wind {
+            direction: 210,
+            speed: Knots(9),
+            gust: Some(Knots(19)),
+            variable: Some(60..=130),
+        },
+        visibility: 5000,
+        percipitation: Some(Percipitation::Rain {
+            intensity: Some(Intensity::Light),
+        }),
+        clouds: vec![
+            CloudLayer {
+                ceiling: 700,
+                cover: Cover::Few,
+                ..Default::default()
+            },
+            CloudLayer {
+                ceiling: 1400,
+                cover: Cover::Broken,
+                significant: Some(CloudSignificant::Cumulonimbus),
+            },
+            CloudLayer {
+                ceiling: 1700,
+                cover: Cover::Broken,
+                ..Default::default()
+            },
+        ],
+        temperature: 2,
+        dew_point: -1,
+        pressure: Hectopascal(1001),
+    };
+    assert_eq!(parser.parse().unwrap(), expected);
 }
